246ac61f7b31462dc6b71d783475c54629e250a1
Dracothehuman <80511020+Dracothehuman@users.noreply.github.com>
2022-02-06 11:22:28 +1100

    Add files via upload

---
+++ book.js
@@ -0,0 +1,37 @@
+img="";
+status="";
+
+function preload(){
+    img = loadImage("book.jpg");
+}
+
+function setup(){ 
+    canvas = createCanvas(780, 540);
+    canvas.center()
+    objectDetector=ml5.objectDetector('cocossd', modalLoaded);
+    document.getElementById("status").innerHTML ="Status:Detecting Image";
+}
+
+function draw(){
+    image(img , 0 , 0, 780, 540);
+    fill("#FF0000");
+    text("Book", 45, 75);
+    noFill();
+    stroke("#FF0000");
+    rect(40, 60, 450, 375);
+}
+function back(){
+    window.location="index.html";
+}
+function modalLoaded(){
+    console.log("Modal has loaded");
+    status= true;
+    objectDetector.detect(img, gotResult);
+}
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    
+}
---
+++ main.js
@@ -0,0 +1,12 @@
+function key(){
+    window.location="key.html";
+}
+function Poke(){
+    window.location="pokemon.html";
+}
+function book(){
+    window.location="book.html";
+}
+function cup(){
+    window.location="cup.html";
+}
